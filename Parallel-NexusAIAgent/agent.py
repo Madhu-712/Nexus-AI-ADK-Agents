e452767ec4353f635c9b4c719a06067c90f3d99f
Madhu-712 <88141363+Madhu-712@users.noreply.github.com>
2025-11-02 20:38:23 +0530

    Update agent.py

--- Parallel-NexusAIAgent/agent.py
+++ Parallel-NexusAIAgent/agent.py
@@ -122,5 +122,23 @@ sequential_pipeline_agent=SequentialAgent(
 )
 
 
-root_agent =sequential_pipeline_agent
-print("🤖 Agent team supercharged with a ParallelAgent workflow!")
+
+
+
+
+
+
+
+
+root_agent = Agent(
+   model='gemini-2.5-flash',
+   name='sequential_pipeline_agent',
+   description='A helpful assistant for user questions.Act as a Agent team supercharged with a ParallelAgent workflow!',
+      tools=[
+         FunctionTool(get_SalesTAXinfo), 
+         FunctionTool(get_VATinfo),
+         FunctionTool(get_commodityprice),
+         LangchainTool(langchain_wikipedia_tool),
+         AgentTool(agent=google_search_agent),
+        ]
+)
